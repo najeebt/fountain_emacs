025e983ea519d7099c2ae24c212f84556909c5d7
najeeb <najeebtarazi@gmail.com>
2014-01-21 14:14:22 -0800

    - first pass

---
+++ fountain.el
@@ -0,0 +1,44 @@
+(defvar fountain-mode-hook nil)
+(add-to-list 'auto-mode-alist '("\\.fountain\\'" . fountain-mode))
+
+(add-hook 'text-mode-hook 'visual-line-mode)
+
+(set-face-attribute 'font-lock-warning-face nil :weight 'bold :foreground "black")
+
+(defconst wpdl-font-lock-keywords-1
+  (list
+   ; scene headings
+   '("^\\(?:\\(?:EXT\\|INT\\|\\(?:ex\\|in\\)t\\)\\.\\)\.\*\$" . font-lock-function-name-face)
+   ; **text** => bold
+   '("\\*\\*\.\*\\*\\*" . font-lock-warning-face))
+  "Minimal highlighting expressions for WPDL mode.")
+
+(defconst wpdl-font-lock-keywords-2
+  (append wpdl-font-lock-keywords-1
+		  (list
+		   ; dialogue-related matches, w/parentheticals
+		   '("^[A-Z\s\(\)\\.\'#1-9,]\*\$" . font-lock-keyword-face)
+		   '("^\(\.\*\)\$" . font-lock-constant-face)))
+  "Additional Keywords to highlight in WPDL mode.")
+
+(defconst wpdl-font-lock-keywords-3
+  (append wpdl-font-lock-keywords-2
+		  (list
+		   '("^\.\*:\$" . font-lock-constant-face)))
+  "Balls-out highlighting in WPDL mode.")
+
+(defvar wpdl-font-lock-keywords wpdl-font-lock-keywords-3
+  "Default highlighting expressions for WPDL mode.")
+
+(defun fountain-mode ()
+  (interactive)
+  (kill-all-local-variables)
+  ;;(use-local-map fountain-mode-map)
+  ;; Set up font-lock
+  (set (make-local-variable 'font-lock-defaults) '(wpdl-font-lock-keywords))
+  (setq major-mode 'fountain-mode)
+  (setq mode-name "fountain")
+  (visual-line-mode t)
+  (run-hooks 'fountain-mode-hook))
+
+(provide 'fountain-mode)
